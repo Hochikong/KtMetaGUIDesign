a8732d838a844eed3ba6b13c464442f70c59e2e3
Hochikong <ckhoidea@hotmail.com>
2021-01-30 18:33:55 +0800

    inital commit

---
+++ src/main/java/io/github/hochikong/ktmeta/testOnly/JSplitPaneHideTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.hochikong.ktmeta.testOnly;
+
+/**
+ *
+ * @author ckhoi
+ */
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class JSplitPaneHideTest extends JFrame {
+   private JButton leftBtn, rightBtn;
+   private JSplitPane jsp;
+   public JSplitPaneHideTest() {
+      setTitle(" JSplitPaneHide Test");
+      leftBtn = new JButton("Left Button");
+      rightBtn = new JButton("Right Button");
+      jsp = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftBtn, rightBtn);
+      jsp.setResizeWeight(0.5);
+      // Implemention code to hide left pane or right pane
+      ActionListener actionListener = new ActionListener() {
+         private int loc = 0;
+         public void actionPerformed(ActionEvent ae) {
+            JButton source = (JButton)ae.getSource();
+            if(jsp.getLeftComponent().isVisible() && jsp.getRightComponent().isVisible()) {
+               loc = jsp.getDividerLocation();
+               jsp.setDividerSize(0);
+               jsp.getLeftComponent().setVisible(source == leftBtn);
+               jsp.getRightComponent().setVisible(source == rightBtn);
+            } else {
+               jsp.getLeftComponent().setVisible(true);
+               jsp.getRightComponent().setVisible(true);
+               jsp.setDividerLocation(loc);
+               jsp.setDividerSize(10);
+            }
+         }
+      };
+      rightBtn.addActionListener(actionListener);
+      leftBtn.addActionListener(actionListener);
+      add(jsp, BorderLayout.CENTER);
+      setSize(400, 300);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setLocationRelativeTo(null);
+      setVisible(true);
+   }
+   public static void main(String[] args) {
+      new JSplitPaneHideTest();
+   }
+}
---
+++ src/main/java/io/github/hochikong/ktmeta/testOnly/TreeWithMultiDiscontiguousSelections.java
@@ -0,0 +1,41 @@
+package io.github.hochikong.ktmeta.testOnly;
+
+import java.awt.Dimension;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.tree.*;
+
+public class TreeWithMultiDiscontiguousSelections {
+
+    public static void main(String[] argv) {
+        JTree tree = new JTree();
+        tree.getSelectionModel().setSelectionMode(TreeSelectionModel.DISCONTIGUOUS_TREE_SELECTION);
+        int treeSelectedRows[] = {3, 1};
+        tree.setSelectionRows(treeSelectedRows);
+        TreeSelectionListener treeSelectionListener = new TreeSelectionListener() {
+
+            @Override
+            public void valueChanged(TreeSelectionEvent treeSelectionEvent) {
+                JTree treeSource = (JTree) treeSelectionEvent.getSource();
+                System.out.println("<-------------------->");
+                System.out.println("Min: " + treeSource.getMinSelectionRow());
+                System.out.println("Max: " + treeSource.getMaxSelectionRow());
+                System.out.println("Lead: " + treeSource.getLeadSelectionRow());
+                for (int x : treeSource.getSelectionRows()) {
+                    System.out.println(x);
+                }
+            }
+        };
+        tree.addTreeSelectionListener(treeSelectionListener);
+        JFrame frame = new JFrame("JTree With Multi-Discontiguous selection");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new JScrollPane(tree));
+        frame.setPreferredSize(new Dimension(380, 320));
+        frame.setLocation(150, 150);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private TreeWithMultiDiscontiguousSelections() {
+    }
+}
